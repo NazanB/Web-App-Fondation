37244d1ddbdba7f6e47c7628a6b11ecae2ce90b0
Nazan Bas <n-8888@t-online.de>
2023-06-05 15:22:03 +0200

    Solution new Kata

---
+++ FastCheapGood/script.js
@@ -0,0 +1,34 @@
+"use strict";
+/*
+let lastActivitySwitch = null;
+
+function tasteToggle() {
+  const fastCheckbox = document.getElementById("fast");
+  const cheapCheckbox = document.getElementById("cheap");
+  const goodCheckbox = document.getElementById("good");
+
+  fastCheckbox.addEventListener("click", () => {
+    if (fastCheckbox.checked) {
+      lastActivitySwitch = "fast";
+      cheapCheckbox.checked = false;
+      goodCheckbox.checked = false;
+    }
+  });
+
+  cheapCheckbox.addEventListener("click", () => {
+    if (cheapCheckbox.checked) {
+      lastActivitySwitch = "cheap";
+      fastCheckbox.checked = false;
+      goodCheckbox.checked = false;
+    }
+  });
+
+  goodCheckbox.addEventListener("click", () => {
+    if (goodCheckbox.checked) {
+      lastActivitySwitch = "good";
+      fastCheckbox.checked = false;
+      cheapCheckbox.checked = false;
+    }
+  });
+}
+*/

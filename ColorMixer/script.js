8622e31c00da3a7657c24c567b8624f8f0c0d135
Nazan Bas <n-8888@t-online.de>
2023-06-07 00:20:09 +0200

    ColorMixer : not yet done

---
+++ ColorMixer/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+let rangeInputRed = document.getElementById("red");
+let rangeInputGreen = document.getElementById("green");
+let rangeInputBlue = document.getElementById("blue");
+
+let redCurrentValue = rangeInputRed.addEventListener(
+  "change",
+  () => {
+    changeColor(redCurrentValue, greenCurrentValue, blueCurrentValue);
+    return document.getElementById("red").value;
+  },
+  false
+);
+
+let greenCurrentValue = rangeInputGreen.addEventListener(
+  "change",
+  () => {
+    changeColor(redCurrentValue, greenCurrentValue, blueCurrentValue);
+    return document.getElementById("green").value;
+  },
+  false
+);
+
+let blueCurrentValue = rangeInputBlue.addEventListener(
+  "change",
+  () => {
+    changeColor(redCurrentValue, greenCurrentValue, blueCurrentValue);
+    return document.getElementById("blue").value;
+  },
+  false
+);
+
+function changeColor(r, g, b) {
+  const color =
+    "#" + rangeValueToHex(r) + rangeValueToHex(g) + rangeValueToHex(b);
+  document.body.style.backgroundColor = color;
+  //document.body.style.backgroundColor = # " + r + "," + g + "," + b + ";
+  console.log("sdgnf");
+}
+function rangeValueToHex(value) {
+  value = Number.parseInt(value);
+  return ("0" + value.toString(16)).substr(-2);
+}

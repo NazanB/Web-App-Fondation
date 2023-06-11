610ad69f221dea7dbe42745614926d518e324c63
Nazan Bas <n-8888@t-online.de>
2023-06-11 23:57:08 +0200

    Kata : update

--- ColorMixer/script.js
+++ ColorMixer/script.js
@@ -1,56 +1,10 @@
 "use strict";
-/*
 let rangeInputRed = document.getElementById("red");
 let rangeInputGreen = document.getElementById("green");
 let rangeInputBlue = document.getElementById("blue");
 
-let redCurrentValue = rangeInputRed.addEventListener(
-  "change",
-  () => {
-    changeColor(redCurrentValue, greenCurrentValue, blueCurrentValue);
-    return document.getElementById("red").value;
-  },
-  false
-);
-
-let greenCurrentValue = rangeInputGreen.addEventListener(
-  "change",
-  () => {
-    changeColor(redCurrentValue, greenCurrentValue, blueCurrentValue);
-    return document.getElementById("green").value;
-  },
-  false
-);
-
-let blueCurrentValue = rangeInputBlue.addEventListener(
-  "change",
-  () => {
-    changeColor(redCurrentValue, greenCurrentValue, blueCurrentValue);
-    return document.getElementById("blue").value;
-  },
-  false
-);
-
-function changeColor(r, g, b) {
-  const color =
-    "#" + rangeValueToHex(r) + rangeValueToHex(g) + rangeValueToHex(b);
-  document.body.style.backgroundColor = color;
-  //document.body.style.backgroundColor = # " + r + "," + g + "," + b + ";
-  console.log("sdgnf");
-}
-function rangeValueToHex(value) {
-  value = Number.parseInt(value);
-  return ("0" + value.toString(16)).substr(-2);
-}
-
-*/
-
 document.addEventListener("input", () => {
-  let rangeInputRed = document.getElementById("red");
-  let rangeInputGreen = document.getElementById("green");
-  let rangeInputBlue = document.getElementById("blue");
-
-  colorChange(
+  UpdateDisplay(
     rangeInputRed.valueAsNumber,
     rangeInputGreen.valueAsNumber,
     rangeInputBlue.valueAsNumber
@@ -61,3 +15,21 @@ function colorChange(red, green, blue) {
   document.body.style.backgroundColor =
     "rgb(" + red + "," + green + "," + blue + ")";
 }
+function rangeValueToHex(value) {
+  value = Number.parseInt(value);
+  return value.toString(16).length < 2
+    ? "0" + value.toString(16)
+    : value.toString(16);
+}
+
+function UpdateDisplay(r, g, b) {
+  colorChange(r, g, b);
+  document.getElementById("color-value").innerHTML =
+    "#" + rangeValueToHex(r) + rangeValueToHex(g) + rangeValueToHex(b);
+}
+
+UpdateDisplay(
+  rangeInputRed.valueAsNumber,
+  rangeInputGreen.valueAsNumber,
+  rangeInputBlue.valueAsNumber
+);

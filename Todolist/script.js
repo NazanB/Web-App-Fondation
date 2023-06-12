16e8238629c4cfdf75bbec66450e2731518259de
Nazan Bas <n-8888@t-online.de>
2023-06-12 13:20:21 +0200

    cosmetics update

--- Todolist/script.js
+++ Todolist/script.js
@@ -1,31 +1,42 @@
 "use strict";
 
-let myInput = document.getElementById("new-todo");
-let btn = document.getElementById("add-btn");
-let liste = document.getElementById("list");
-let btnRemove = document.getElementById("remove-btn");
+const myInput = document.getElementById("new-todo");
+const btn = document.getElementById("add-btn");
+const liste = document.getElementById("list");
+const btnRemove = document.getElementById("remove-btn");
+const checkList = document.getElementById("todoList");
+
+let filter = document.getElementById("div-filter");
+filter.addEventListener("click", () => {
+  if (document.getElementById("input-all").checked) {
+  } else if (document.getElementById("input-open").checked) {
+  } else if (document.getElementById("input-done").checked) {
+  }
+});
 
 btn.addEventListener("click", () => {
-  let newTodo = document.createElement("li");
+  if (myInput.value.trim().length > 0) {
+    const newTodo = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newTodo.appendChild(checkbox);
 
-  let checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  newTodo.appendChild(checkbox);
+    //console.log(document.getElementById("input-all").checked);
 
-  let todoText = document.createElement("span");
-  todoText.innerText = myInput.value;
-  newTodo.appendChild(todoText);
+    const todoText = document.createElement("span");
+    todoText.innerText = myInput.value;
+    newTodo.appendChild(todoText);
 
-  liste.appendChild(newTodo);
-  myInput.value = "";
+    liste.appendChild(newTodo);
+    myInput.value = "";
+  }
 });
 
 btnRemove.addEventListener("click", () => {
-  let checkboxes = document.querySelectorAll("#list input[type='checkbox']");
-
+  const checkboxes = document.querySelectorAll("#list input[type='checkbox']");
   checkboxes.forEach((checkbox) => {
     if (checkbox.checked) {
-      let listItem = checkbox.parentNode;
+      const listItem = checkbox.parentNode;
       listItem.parentNode.removeChild(listItem);
     }
   });
